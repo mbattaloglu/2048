2bc6a38d7aa554fa9dba33d75c2460b2717d75b1
mbattaloglu <mmbattaloglu@gmail.com>
2023-04-02 02:25:04 +0300

    Tile States

---
+++ Assets/Scripts/TileState.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewTileState", menuName = "Tile State")]
+public class TileState : ScriptableObject
+{
+    public Color backgroundColor;
+    public Color textColor;
+
+} 
